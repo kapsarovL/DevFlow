b62a7e8af986761ac77ecfed69da2a20921e3c7c
kapsarovL <kapsarovlazar@gmail.com>
2024-11-04 23:09:32 +0100

    Setup (auth) & (root), Create form component, & install auth.js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,6 +75,10 @@ const config: Config = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      backgroundImage: {
+        "auth-dark": 'url("/images/auth-dark.png")',
+        "auth-light": 'url("/images/auth-light.png")',
+      },
       boxShadow: {
         "light-100":
           "0px 12px 20px 0px rgba(184, 184, 184, 0.03), 0px 6px 12px 0px rgba(184, 184, 184, 0.02), 0px 2px 4px 0px rgba(184, 184, 184, 0.03)",
