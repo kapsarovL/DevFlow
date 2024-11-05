2fcae57293cc22c5485bf53a45f1a78eba092b5e
kapsarovL <kapsarovlazar@gmail.com>
2024-11-05 23:51:36 +0100

    Setup Auth Layouts, Social Auth Form, Next Auth, OAuth with Google and GitHub Providers

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,28 @@
-const Home = () => {
-  return <h1 className="text-3xl font-bold text-white">Home</h1>;
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+
+  console.log(session);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-white">Home</h1>
+
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log out</Button>
+      </form>
+    </>
+  );
 };
 
 export default Home;
