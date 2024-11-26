f1908084ee30a683389f87bc09ee6f513905ff98
Lazar Kapsarov <kapsarovlazar@gmail.com>
2024-11-26 20:42:13 +0100

    Implement Local Search Bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from "react";
 
 import LeftSidebar from "@/components/navigation/LeftSidebar";
 import Navbar from "@/components/navigation/navbar";
+import RightSidebar from "@/components/navigation/RightSidebar";
+
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="background-light850_dark100 relative">
@@ -13,6 +15,8 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           {children}
         </section>
+
+        <RightSidebar />
       </div>
     </main>
   );
